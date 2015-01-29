ec8362ff17e017d623e608748f92dcc0c97d5f81
guti15 <guti310@gmail.com>
2015-01-29 13:28:53 -0500

    "renaming folder and adding new folder, using python"

---
+++ project-one/Bloom Filter.py
@@ -0,0 +1,9 @@
+# Auhor: Robert Gutierrez
+# Obj: Building a Bloom
+# Bloom Filter:  A data structure that tells you quickly, wheather an element is in the set.  while being memory efficient.
+
+
+
+
+
+
