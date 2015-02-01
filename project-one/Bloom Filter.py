fb12e9b51892b28b23c964e764f808a8cdbef220
guti15 <guti310@gmail.com>
2015-02-01 01:46:59 -0500

    'updating with deleating some files and adding some work on the Bloom Filter'

--- project-one/Bloom Filter.py
+++ project-one/Bloom Filter.py
@@ -1,7 +1,39 @@
 # Auhor: Robert Gutierrez
 # Obj: Building a Bloom
 # Bloom Filter:  A data structure that tells you quickly, wheather an element is in the set.  while being memory efficient.
+# create a class Bloom filter:
 
+from bitarray import bitarray
+import mmh3
+
+class Bloom_Filter:
+    def __init__(self,array, percantage-postive, size, Hash_count):
+        self.size       = size
+        self.Barray     = bitarray(size)
+        self.Percentage = Percentage-postive
+        self.Hash_count = Hash_count
+        
+        self.Barray.setcall(0) 
+
+    def add(self, string):
+        for seed in xrange(self.hash_count):
+            total = mmh3.hash(string, seed) % self.size 
+    
+
+    def lookup(self, size):
+
+
+        
+def main():
+
+     
+
+
+
+
+
+if __name__=='__main__':
+    main()
 
 
 
