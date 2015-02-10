47ad032db02033649536f159f18c97d6845f1519
guti15 <guti310@gmail.com>
2015-02-09 23:46:06 -0500

    Fully running Project 1, added a file with vocab to read in as well

--- project-one/Bloom Filter.py
+++ project-one/Bloom Filter.py
@@ -2,7 +2,11 @@
 # Obj: Building a Bloom
 # Bloom Filter:  A data structure that tells you quickly, wheather 
 # an elment might be in the set, or not in the set at all.
-# libraries:  [md5: a cryptic hasing library] [bitarray: create low memorey consuming array]
+# Hash Equation: gi(x) = h1(x) + ih2(x) % m,
+# m = bits in array, h = hashfunction, i = number of hashes 
+# libraries:  [md5: a cryptic hasing library]
+# [bitarray: create low memorey consuming array]
+# 
 # input: string 
 # output: "we might have your informataion"  or " sorry no data found" 
 
@@ -15,56 +19,75 @@ from bitarray import bitarray
 import math 
 import mmh3
 import hashlib
-import md5
+
 
 class BloomFilter:
     
     def __init__(self, size, num_hash):
-        self.size = size        # init number of bits
-        self.num_hash = num_hash #  init number of hashes 
+        self.size = size                  # init number of bits
+        self.num_hash = num_hash          #  init number of hashes 
         self.bloom_array = bitarray(size) #  make an array with given size
-        self.bloom_array.setall(0)       #  bit array starts with all 0's
+        self.bloom_array.setall(0) #  bit array starts with all 0's
 
 
     def encrypt(self, string):
-        crypt = hashlib.sha1()
-        crypt.update(string)
-        key = crypt.hexdigest() #  gives a hexadecimal number
-        key = int(key, 16)      #  convert the number 
-        key = key % self.size        #  location in bit array.
+        crypt_sha1 = hashlib.sha1()
+        crypt_sha1.update(string)
+        key = crypt_sha1.hexdigest() #  gives a hexadecimal number
+        key = int(key, 16)           #  convert the number
+        crypt_mmh3 = mmh3.hash128(string)
         
-        return key
+        # Hash Equation: gi(x) = h1(x) + ih2(x) % m,
+        for i in range(self.num_hash):
+            location = (key + i * crypt_mmh3) % self.size
+            self.bloom_array[location] = True # mark the unique key location
 
+        # end encrypt
+
+    def decrypt(self, string):
+        crypt_sha1 = hashlib.sha1()
+        crypt_sha1.update(string)
+        key = crypt_sha1.hexdigest() #  gives a hexadecimal number
+        key = int(key, 16)           #  convert the number
+        crypt_mmh3 = mmh3.hash128(string)
+    
+        for i in range(self.num_hash ):
+            
+            location = (key + i * crypt_mmh3) % self.size
+            if self.bloom_array[location] == False:
+                return False    
+            
+        return True             # return True if found otherwise breaks
+    
     
     def lookup(self, string):
-        am_I_here = self.encrypt(string) 
+        am_I_here = True
+        am_I_here = self.decrypt(string) # create hash key 
             
-        if (self.bloom_array[am_I_here] == 0) :
-            return False
-            
-        else:
+        if am_I_here == True:
             return True
+        
+        else:
+            return False 
             
+        # end lookup
 
     def mark(self, string):
-        print string 
-        mark_here = self.encrypt(string)
-        self.bloom_array[mark_here] = 1
-        print "your array has been marked in cell: ", mark_here
-            
-    
+        mark_here = self.encrypt(string) # create hash key 
+        # self.bloom_array[mark_here] = 1  # Mark it with a 1
 
+        # end mark
 
     
 def false_postive_rate(bits_Filter, num_elemt , num_hash):
-    FPR = ( 1 - math.e**((-num_hash * num_elemt / bits_Filter)))** num_hash
+    FPR = (1 - math.e**((-num_hash * num_elemt / bits_Filter)))** num_hash
     FPR = round(FPR, 5) * 100
 
     return FPR
 
     
 def number_of_bits( items_expect , accpt_fpostive):    
-    bits_needed  = -items_expect*math.log(accpt_fpostive)/(math.log(2)**2)
+    bits_needed  = (-items_expect*math.log(accpt_fpostive))/(math.log(2)**2)
     
     return bits_needed
 
@@ -77,56 +100,44 @@ def num_hash_apply(bits_needed,items_expect):
     
     
 def main():
+    # nips full paper apprx: 1,900,000 words
+    Estimate_Array = 2000000   
+    FALSE_POSTIVE  = .01        # with a 1% error 
 
-    Estimate_Array = 4294967296
-    FALSE_POSTIVE  = .01 
-    small_list=['hello', 'world' ,'hi', 'down', 'up', 'right', 'left']
-
-    bits_filter = number_of_bits( Estimate_Array, FALSE_POSTIVE)
+    bits_filter = number_of_bits( Estimate_Array, FALSE_POSTIVE) 
     bits_filter = math.ceil(bits_filter)
     
     how_many_hash = num_hash_apply( bits_filter, Estimate_Array)
-    how_many_hash = math.ceil(how_many_hash)
+    how_many_hash = int(math.ceil(how_many_hash))
 
-    bfilter = BloomFilter( Estimate_Array, how_many_hash)
-
-    # This encrypts the small list above
-    print bfilter.bloom_array[0]
+    bfilter = BloomFilter( Estimate_Array, how_many_hash) # establish Bloom
     
-    crypt = hashlib.sha1()
-    for words in small_list:
-        bfilter.mark(words)
+    read_file = open('./vocab.nips.txt', 'r') #  open the file 
 
-
-
-
-    # SMALL TEST and it RUNNSS !!
-    #  Get more Data  clean up and improve if possible 
+    for words in read_file:     #  read list of words
+        words = words.rstrip("\n") # take out return char
+        bfilter.mark(words)        #  update the filter by marking it
+        
     
-    exist = bfilter.lookup('hel')
-    
-    if exist == True :
-        print 'YAY I exist! '
-    else:
-        print ' you suck at this >:| '
-    
+    ASK = True    
+    while ASK == True:
+        find = raw_input("Enter Word: ")
+        visited = bfilter.lookup(find)
+            
+        if visited == True:
+            print "It's compleatly possible that it may exist\n"
+            ASK = input("To continue press 1, to end press 0 : ")
+            
+        else:
+            print "Sorry no record of such Word.\n"
+            ASK = input("To continue press 1, to end press 0 : ")
+
+                
+    print "Closing Program..."
+
+        
         
 
-
-        
-    print "Estimated items %d\nBits in Array: %.2f\nNumber of Hash Func: %d" %( Estimate_Array, bits_filter, how_many_hash)
-
-
-
-
-
-    
 if __name__=='__main__':
     main()
-
-
-
-
-
-
 
