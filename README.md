662d9da5855e91a05d8642dd933346688635fb0f
guti15 <guti310@gmail.com>
2015-02-11 23:16:21 -0500

    adding driver file: main.py : Additionally adding more info to Readme.md

--- README.md
+++ README.md
@@ -8,8 +8,36 @@ Repository for Senior Sem  [comp 401](https://github.com/WheatonWHALE/comp401): 
 
 ![Bloom Filter](http://img3.douban.com/view/note/large/public/p8006482.jpg)
 
-This filter is great on memorey, as it test whether or not an item is on the set. The Bloom Filter, searches wheather or not an item is on the array, by marking a bit array. Bloom filter usually contain more than one hash functions, marking the Bit array more than once.
-The Draw Backs:  This data structure fails to tell us wheather the item is in the array, just the possibility that it 'might be' or 'not in the set'.  For more information, and clarity wikipedia has more detailed information [Bloom Filter](http://en.wikipedia.org/wiki/Bloom_filter). 
+This filter is great on memory, as it test whether or not an item is on the set. The Bloom Filter, searches whether or not an item is on the array, by marking a bit array. Bloom filter usually contain more than one hash functions, marking the Bit array more than once.
+The Draw Backs:  This data structure fails to tell us weather the item is in the array, just the possibility that it 'might be' or 'not in the set'.  For more information, and clarity Wikipedia has more detailed information [Bloom Filter](http://en.wikipedia.org/wiki/Bloom_filter). 
+
+#####Functionality
+Functionality of Bloom Filter:
+* *mark* - reads in a string, which is then _Hashed_ with a unique code
+* *lookup* - searches for a string in the Bloom Filter. Checks every possible index
+* *number of bits* - Calculates how big the _Bit Array_ should be. Depends on your ideal False Positive.
+* *Num hash apply* - Calculate the adequate number of _Hash Functions_
+* False postive rate - Calculates the false postive given _bits in filter, number of hashes, number of elements_
+
+
+#### Complexity Analysis
+**Bloom Filter mark:**
+
+
+**Bloom Filter lookup:**
+
+
+
+
+####Resources Used:
+[Wikipedia](http://en.wikipedia.org/wiki/Bloom_filter)
+[Python 2.7 documentation](https://docs.python.org/2.7/)
+[dev/maverick Blog](http://ilyasterin.com/blog/2010/02/implementing-bloom-filter-with-a-murmur-hash-function.html)
+
+
+
+
+
 
 
 
--- project1/main.py
+++ project1/main.py
@@ -1,44 +1,61 @@
 from BloomFilter import number_of_bits, num_hash_apply,num_hash_apply
 from BloomFilter import BloomFilter
 import math
-
+#--------------------------------------------------------------------------------
 def main():
+    
     # nips full paper apprx: 1,900,000 words
     Estimate_Array = 2000000   
     FALSE_POSTIVE  = .01        # with a 1% error
+    COUNTER_SKIP = 0            # A counter reset every 3,000 words
+    pos_words = []
+    neg_words = []
 
-
-    bits_filter = number_of_bits( Estimate_Array, FALSE_POSTIVE) 
+    bits_filter = number_of_bits(Estimate_Array, FALSE_POSTIVE)
     bits_filter = math.ceil(bits_filter) # number is a float, so we round 
     
     how_many_hash = num_hash_apply( bits_filter, Estimate_Array)
     how_many_hash = int(math.ceil(how_many_hash)) # round up
-
-    bfilter = BloomFilter( Estimate_Array, how_many_hash) # establish Bloom
     
-    read_file = open('./data_set/vocab.nips.txt', 'r') #  open the file 
+    bfilter = BloomFilter( Estimate_Array, how_many_hash)
+    read_file = open('./data_set/vocab.nips.txt', 'r') 
 
-    for words in read_file:     #  read list of words
-        words = words.rstrip("\n") # take out return char
-        bfilter.mark(words)        #  update the filter by marking it
-        
-    
-    ASK = True                  # ask user to search for words
-    while ASK == True:
-        find = raw_input("Enter Word: ")
-        visited = bfilter.lookup(find)
+    # READ FILE 
+    for words in read_file:     # read list of words
+        if COUNTER_SKIP!= 100:
+            words = words.rstrip("\n") # take out return '\n'
+            bfilter.mark(words) # update the filter by marking it
             
-        if visited == True:
-            print "It's compleatly possible that it may exist\n"
-            ASK = input("To continue press 1, to end press 0 : ")
+            if COUNTER_SKIP ==  99:  
+                pos_words.append(words) 
+                
+            COUNTER_SKIP += 1
             
         else:
-            print "Sorry no record of such Word.\n"
-            ASK = input("To continue press 1, to end press 0 : ")
+            words = words.rstrip("\n") # take out return char
+            neg_words.append(words) 
+            COUNTER_SKIP = 0    #  Don't add the Word
+            print "this word will not be inserted: " , words
+            
+    # ----------------------------------------------------------------------
+    #  Now to Test our data:
 
-    print "Closing Program..."
+    print '\n'
+    
+    for words in range(20):     # range can only max out at size of array (122)
 
+        piece1 =  "Word in pos array: {0:<15}".format( pos_words[words])
+        piece2 = " Found in list: %r " % (bfilter.lookup(pos_words[words]))
+    
+        piece3 =  "Word in neg array: {0:<15}".format( neg_words[words])
+        piece4 = " Found in list: %r " % (bfilter.lookup(neg_words[words]))
+        
+        print  piece1 + piece2 + "   " + piece3 + piece4
 
+                                
+    print "\nProgram Terminated.\n" 
     
 if __name__=='__main__':
-    main()
+                                                         
+        main()
+
